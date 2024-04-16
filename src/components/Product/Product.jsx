f8d72ce91405102ba4663d502b8d9f6a30f5ee8d
sancty <sanctifieryawmensah55@gmail.com>
2024-04-16 12:04:30 +0200

    [update] Ajout boutton Slide next et prev

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useRef } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
 //
 
 
@@ -24,7 +25,37 @@ export const Card =(props) => {
     )
 }
 
-export const Slide =() => {
+export const CustomPrevArrow =(props)=> {
+    const { onClick } = props;
+    return (
+        <button className="p-4 rounded-full border border-gray-300" onClick={onClick}>
+            <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M16 7.50012H3.83L9.42 1.91012L8 0.500122L0 8.50012L8 16.5001L9.41 15.0901L3.83 9.50012H16V7.50012Z" fill="#B9B9B9"/>
+            </svg>
+        </button>
+    )
+}
+
+
+export const CustomNextArrow =(props)=> {
+    const { onClick } = props;
+    return (
+        <button className="rounded-full bg-blue-400 p-4 " onClick={onClick}>
+            <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 0.500122L6.59 1.91012L12.17 7.50012H0V9.50012H12.17L6.59 15.0901L8 16.5001L16 8.50012L8 0.500122Z" fill="white"/>
+            </svg>
+        </button>
+    )
+    
+}
+
+
+
+
+
+
+
+export const Slide =React.forwardRef((props,ref) =>{
 
     const cardData = [
         {
@@ -56,16 +87,19 @@ export const Slide =() => {
     
 
     const settings = {
-        dots : true,
+        arrows: false, // Masquer les flèches par défaut
+        dots: false, // Masquer les points indicatifs par défaut
         indfinite : true,
         speed:500,
         slidesToShow:4,
         slidesToScroll:1,
         centerMode:true,
+        nextArrow:<CustomNextArrow />,
+        prevArrow :<CustomPrevArrow />,
     }
 
     return(
-        <Slider {...settings}>
+        <Slider ref={ref} {...settings}>
 
             {cardData.map((card ,index) => (
                 <div key={index} className="px-4">
@@ -79,13 +113,15 @@ export const Slide =() => {
             ))}
         </Slider>    
     );
-}
-
+});
 
 
 
 
 export const Product = () => {
+
+    const sliderRef = useRef() ;// creation d'une réference au composant Slider 
+
 
     return (
         <>
@@ -97,24 +133,16 @@ export const Product = () => {
                         <h1 className="pb-4 text-3xl">Popular Destinations</h1>
                         <p>See our popular destinations that our client choose.</p>
                     </div>
-
+                     {/* Passage de la méthode slickNext au bouton next  pour defiler le caroussel vers l'avant  */}
                     <div className="flex justify-center items-center gap-4 mr-28">
-                        <div className="p-4 rounded-full border border-gray-300">
-                            <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16 7.50012H3.83L9.42 1.91012L8 0.500122L0 8.50012L8 16.5001L9.41 15.0901L3.83 9.50012H16V7.50012Z" fill="#B9B9B9"/>
-                            </svg>
-                        </div>
-                        <div className="rounded-full bg-blue-400 p-4">
-                            <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M8 0.500122L6.59 1.91012L12.17 7.50012H0V9.50012H12.17L6.59 15.0901L8 16.5001L16 8.50012L8 0.500122Z" fill="white"/>
-                            </svg>
-                        </div>
+                        <CustomPrevArrow onClick={() => sliderRef.current.slickPrev()} />
+                        <CustomNextArrow onClick={() => sliderRef.current.slickNext() } />
                     </div>
 
                 </div>
 
-                <Slide/>
                
+                <Slide ref={sliderRef}/>
             </div>
 
         </>
